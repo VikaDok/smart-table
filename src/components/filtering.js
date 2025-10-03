b7785e531e5d83c6ba1c0d7f9a328f9dedee3b69
Viktoria Dokuchaeva <vika-dok1976@yandex.ru>
2025-10-03 11:58:48 +0300

    коррекция сброса поля фильтрации

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -13,20 +13,27 @@ export function initFiltering(elements) {
     const applyFiltering = (query, state, action) => {
         // код с обработкой очистки поля
         if (action && action.name === 'clear') {
-            elements[action.field].value = '';
+            // Находим поле ввода через parentElement
+        const inputElement = action.parentElement.querySelector('input, select');
+        if (inputElement) {
+            inputElement.value = '';
+            
+            // Триггерим событие input чтобы обновить state и перерендерить таблицу
+            inputElement.dispatchEvent(new Event('input', { bubbles: true }));
         }
-
-        const filter = {};
-        Object.keys(elements).forEach(key => {
-            if (elements[key]) {
-                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
-                    filter[`filter[${elements[key].name}]`] = elements[key].value;
-                }
-            }
-        })
-
-        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
     }
+
+    const filter = {};
+    Object.keys(elements).forEach(key => {
+        if (elements[key]) {
+            if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
+                filter[`filter[${elements[key].name}]`] = elements[key].value;
+            }
+        }
+    })
+
+    return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
+}
 
     return {
         updateIndexes,
